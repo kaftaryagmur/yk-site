946677e556f790213f7ed88c1a544a94ca009de5
kaftaryagmur <kaftaryagmur@gmail.com>
2023-07-18 17:58:39 +0300

    slider ve dark/light mode eklendi

---
+++ script.js
@@ -0,0 +1,34 @@
+/*---------------------arrow start--------------------*/
+const arrows = document.querySelectorAll(".arrow");
+const documentList = document.querySelector(".documents-list")
+
+arrows.forEach((arrow, i) => {
+    let clickCounter = 0;
+    const imageItem = documentList.querySelectorAll("img").length;
+    arrow.addEventListener("click", function () {
+        clickCounter++;
+        if (imageItem - (1 + clickCounter) >= 0) {
+            documentList.style.transform = `translateX(${documentList.computedStyleMap().get("transform")[0].x.value - 300}px)`
+        }
+        else {
+            documentList.style.transform = "translateX(0)";
+        }
+    });
+
+});
+/*---------------------arrow end--------------------*/
+
+
+/*---------------------dark mode start--------------------*/
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.navbar,.sidebar,.sidebar i,.toggle,.toggle-ball,.about-container,.education-container,.about-text-container,.date,.education-picture-container,.featured-content");
+
+ball.addEventListener("click", function () {
+    items.forEach((item) => item.classList.toggle("active"))
+});
+
+/*---------------------dark mode end--------------------*/
+
+
+
